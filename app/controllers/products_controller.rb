4d22a0f136ba3683ebaa41bc0dc3baa7179aeeb9
Francois Mizrahi <francois.mizrahi@gmail.com>
2019-03-05 17:23:15 +0100

    change date form and update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   def index
+    @booking = Booking.find(params[:booking_id])
     @products = Product.all
     #For Filter Sidebar
     @products = @products.where(product_category: params[:product_category]) if params[:product_category].present?
@@ -7,4 +8,9 @@ class ProductsController < ApplicationController
     @products = @products.where(material: params[:material]) if params[:material].present?
   end
 
+  private
+
+  # def booking_params
+  #   params.require(:booking).permit(:booking_id)
+  # end
 end
