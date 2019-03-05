7bbf82bead797348ecab882d6c83a228de498172
Yukari Yamamoto <yukariemma@gmail.com>
2019-03-05 17:49:41 +0100

    add filter by room

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     #For Filter Sidebar
+    @products = @products.joins(rooms: :product_rooms).where(rooms: { name: params[:name] }) if params[:name].present?
     @products = @products.where(product_category: params[:product_category]) if params[:product_category].present?
     @products = @products.where(style: params[:style]) if params[:style].present?
     @products = @products.where(material: params[:material]) if params[:material].present?
