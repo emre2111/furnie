6a929f61b20c66d50b9b93dc0f3454dd10ffdbec
Yukari Yamamoto <yukariemma@gmail.com>
2019-03-07 10:40:25 +0100

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require './lib/tasks/scrape.rb'
 
-scraper.each do
 
 puts 'Cleaning database...'
 
