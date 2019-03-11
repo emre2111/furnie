928d5b3aede5e2a45e92eaf4a03d435a0256ef15
Emre Yilmaz <emre211@hotmail.com>
2019-03-11 12:40:49 +0100

    delete

--- lib/tasks/scrape.rb
+++ lib/tasks/scrape.rb
@@ -11,12 +11,13 @@ def scraper(url)
     name = product.search('.article-tile__name').first.text.split[1]
     price_cents = product.search('.article__price-wrap .article__price').first.text.split[0]
     price_cents = 1000 if price_cents == 'ab'
+
     photo = product.search('.article-tile__image').first['data-echo']
 
 
     outputs << {
       :name => name,
-      :price_cents => price_cents.to_f,
+      :price_cents => price_cents,
       :photo => photo
     }
   end
