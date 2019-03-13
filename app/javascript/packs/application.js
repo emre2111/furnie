b88614408fd7b7603d5d3c03e64228fffd405e00
Yukari Yamamoto <yukariemma@gmail.com>
2019-03-13 19:09:18 +0100

    fixed js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,37 +7,14 @@ import  { searchButton }  from "./event-search";
 
 import {eventActive} from "../plugins/event_active";
 import {eventReload} from "./event_reload";
+import {itemCount} from "./addCount";
 
 import  { bookRandom }  from "./event_booking_random";
 
-// import { addCount } from "../components/item-count";
+
 
 eventReload();
 eventActive();
 bookRandom();
-// addCount();
+itemCount();
 
-// To add badge
-document.getElementById("itemCount").innerText = document.getElementById("itemCount").innerText;
-let itemCount = parseInt(document.getElementById("itemCount").textContent) + 1;
-const addCartButton = document.querySelector(".btn-to-cart");
-if(addCartButton){
-  addCartButton.addEventListener("click", addCount);
-}
-
-const numItem = document.getElementById("itemCount");
-  if(numItem.innerText === "0"){
-    numItem.style.display = 'none';
-}
-
-function addCount(e) {
-  // e.preventDefault();
-
-  // increment badge number
-  function incrementValue() {
-    const numItem = document.getElementById("itemCount");
-    numItem.style.display = 'block';
-    numItem.innerText = itemCount++;
-  };
-  incrementValue();
-};
