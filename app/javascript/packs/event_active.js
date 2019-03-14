10d562eba88b05a7ccc72c346eb05df38067382a
Francois Mizrahi <francois.mizrahi@gmail.com>
2019-03-14 18:34:59 +0100

    event random green section default

--- app/javascript/packs/event_active.js
+++ app/javascript/packs/event_active.js
@@ -2,15 +2,15 @@ import {itemCount} from "./addCount";
 
 const eventActive = () => {
   let before;
-  const card = document.querySelectorAll(".random-card");
+  const cards = document.querySelectorAll(".random-card");
+
 
     function handleActive(e){
-        if(before && this === before) return;
-        if(before) before.classList.remove("active-border");
+      console.log(cards)
+      cards.forEach(card => card.classList.remove("active-border"));
         this.classList.add("active-border");
-        before =  this;
     }
-    card.forEach(card => card.addEventListener("click", handleActive));
+    cards.forEach(card => card.addEventListener("click", handleActive));
 }
 
 export  {eventActive};
